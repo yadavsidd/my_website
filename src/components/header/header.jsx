d419e4ba7534495e58ae0c353b660d8f54f33f6c
Siddhant Yadav <76056721+yadavsidd@users.noreply.github.com>
2025-07-11 13:46:08 +0530

    revert test

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,7 +10,7 @@ const Header = () =>  {
     return (
         <header className="header">
             <nav className='nav container'>
-                <a href='index.html' className='nav__logo'>Siddhanttttt</a>
+                <a href='index.html' className='nav__logo'>Siddhant</a>
                 
                 <div className={Toggle ? "nav__menu show-menu" :
                 "nav__menu"}>
